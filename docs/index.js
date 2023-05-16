54a4fd34d906b483a46c3a16d8af2d5dbf3082f3
ArchedEnemy <132998218+ArchedEnemy@users.noreply.github.com>
2023-05-16 22:22:49 +1200

    test other url with basic output

--- docs/index.js
+++ docs/index.js
@@ -15,7 +15,7 @@ async function startApplication() {
   self.pyodide.globals.set("sendPatch", sendPatch);
   console.log("Loaded!");
   await self.pyodide.loadPackage("micropip");
-  const env_spec = ['https://cdn.holoviz.org/panel/0.14.4/dist/wheels/bokeh-2.4.3-py3-none-any.whl', 'https://cdn.holoviz.org/panel/0.14.4/dist/wheels/panel-0.14.4-py3-none-any.whl', 'pyodide-http==0.1.0', 'datetime', 'holoviews>=1.15.4', 'hvplot', 'pandas']
+  const env_spec = ['https://cdn.holoviz.org/panel/0.14.4/dist/wheels/bokeh-2.4.3-py3-none-any.whl', 'https://cdn.holoviz.org/panel/0.14.4/dist/wheels/panel-0.14.4-py3-none-any.whl', 'pyodide-http==0.1.0', 'holoviews>=1.15.4', 'hvplot', 'pandas']
   for (const pkg of env_spec) {
     let pkg_name;
     if (pkg.endsWith('.whl')) {
@@ -51,41 +51,14 @@ import pandas as pd
 import panel as pn
 pn.extension('tabulator', sizing_mode="stretch_width")
 import hvplot.pandas
-import datetime as dt
 
-df = pd.read_csv(r'https://github.com/ArchedEnemy/test1/raw/main/docs/understat_adj.csv',parse_dates = ['date'])
+data_url = "https://cdn.jsdelivr.net/gh/holoviz/panel@master/examples/assets/occupancy.csv"
+data = pd.read_csv(data_url, parse_dates=["date"]).set_index("date")
 
-date_range_slider = pn.widgets.DateRangeSlider(
-    name='Date Range Slider',
-    start=min(df['date']), end=max(df['date']),
-    value=(dt.datetime(2022, 8, 5), max(df['date'])),
-    step=24*3600*1*1000
+pn.Column(
+    "test1",
+    data.head(10)
 ).servable()
-teams = pn.widgets.MultiSelect(options=list(df['team'].unique()),name='Team',value=list(df['team'].unique()),size=10).servable()
-sort = pn.widgets.Select(options=['shot', 'onTarget', 'goal', 'xG', 'xG_Diff','%ShotsOnTarget','GoalsPerShot', 'attAssists', 'Assits', 'xA', 'xA_Diff'],name='Sort by', value='goal').servable()
-
-
-def input_function1(sdate, team, sortby):
-    a=sdate[0]
-    b=sdate[1]
-    df3 = df[(df.date >= a) & (df.date <= b)]
-    df3 = df3[df3['team'].isin(teams.value)]
-    df3['%ShotsOnTarget'] = df3['onTarget']/df3['shot']
-    df3['GoalsPerShot'] = df3['goal']/df3['shot']
-    df4 = df3.groupby(['player','team']).aggregate({'shot':'sum','onTarget':'sum','%ShotsOnTarget':'mean','goal':'sum','xG':'sum','GoalsPerShot':'mean'})
-    df4['xG_Diff'] = df4['goal']-df4['xG']
-    assist_df = df3.groupby(['player_assist']).aggregate({'shot':'sum','goal':'sum','xG':'sum'})
-    assist_df = assist_df.reset_index()
-    assist_df = assist_df.rename(columns={'player_assist': 'player','shot': 'attAssists','goal': 'Assists','xG': 'xA'})
-    assist_df['xA_Diff'] = assist_df['Assists']-assist_df['xA']
-    assist_df['xA'] = assist_df['xA'].fillna(0) 
-    assist_df['Assists'] = assist_df['Assists'].fillna(0) 
-    assist_df['attAssists'] = assist_df['attAssists'].fillna(0) 
-    assist_df['xA_Diff'] = assist_df['xA_Diff'].fillna(0) 
-    df5 = pd.merge(df4, assist_df,how='left', on='player').sort_values(by=sortby, ascending=False).head(20)
-    return df5
-
-pn.Column(pn.Row(teams, pn.Column(date_range_slider, sort)), hvplot.bind(input_function1, date_range_slider, teams, sort)).servable()
 
 await write_doc()
   `
